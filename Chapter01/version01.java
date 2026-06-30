8a3f51402e49dc9cdad826bcb76e92c33397a766
qodbswls0710-create <qodbswls0710@gmail.com>
2026-06-30 11:10:28 +0900

    version control4

--- Chapter01/version01.java
+++ Chapter01/version01.java
@@ -1,5 +1,5 @@
 public class version01 {
     public version01() {
-        System.out.println("Version01 수정");
+        System.out.println("tracked 파일");
     }
 }
---
+++ Chapter01/version02.java
@@ -0,0 +1,5 @@
+public class version02 {
+    public version02() {
+        System.out.println("untracked 파일");
+    }
+}
