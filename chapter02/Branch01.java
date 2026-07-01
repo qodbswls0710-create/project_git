f541e0814acdd09e09eb5c09625eaf57d957823c
qodbswls0710-create <qodbswls0710@gmail.com>
2026-07-01 10:02:37 +0900

    version4

--- chapter02/Branch01.java
+++ chapter02/Branch01.java
@@ -1,7 +1,7 @@
 public class Branch01 {
 
     public Branch01() {
-        System.out.println("메인 브랜치에서 작업");
+        System.out.println("충돌 테스트 삐삐");
         System.out.println("다음 브랜치에서 작업");
     }
 
